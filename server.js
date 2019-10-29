762ced109200cd3d8db5371ee7b2885ea2558e88
dylanwroberts <dylanwroberts99@gmail.com>
2019-10-29 10:36:27 -0500

    added 404 for when you search for a state that doesn't exist

--- server.js
+++ server.js
@@ -153,101 +153,101 @@ app.get('/year/:selected_year', (req, res) => {
 
 // GET request handler for '/state/*'
 app.get('/state/:selected_state', (req, res) => {
-	let currentState = req.params.selected_state;
+    let currentState = req.params.selected_state;
 
-	    db.all('SELECT * FROM Consumption WHERE state_abbreviation = ?', [currentState], (err, rows) => {
-	        if (err) { console.log(err); }
-	        else {
-				console.log("in db");
-	            let count = {
-	                coal: 0,
-	                natural_gas: 0,
-	                nuclear: 0,
-	                petroleum: 0,
-	                renewable: 0
-	            }
+    db.all('SELECT * FROM States', (err, states) => {
+        console.log(states);
+        db.all('SELECT * FROM Consumption WHERE state_abbreviation = ?', [currentState], (err, rows) => {
+            if (err) { console.log(err); }
+            //when the database searches for a state that doesn't exist
+            else if(rows.length == 0) { WriteCustom404Error(res, `no data for the state ${currentState}`) }
+            else {
+                console.log("in db");
+                let count = {
+                    coal: 0,
+                    natural_gas: 0,
+                    nuclear: 0,
+                    petroleum: 0,
+                    renewable: 0
+                }
 
-	            let tableData = '';
+                let tableData = '';
 
-	            //go through each row and total up each catagory and store it in count
-	            rows.forEach(row => {
-	                tableData += `<tr><td>${row.state_abbreviation}</td>`;
+                //go through each row and total up each catagory and store it in count
+                rows.forEach(row => {
+                    tableData += `<tr><td>${row.state_abbreviation}</td>`;
 
-	                for (key in count) {
-	                    count[key] += row[key];
-	                    tableData += `<td>${row[key]}</td>`;
-	                }
+                    for (key in count) {
+                        count[key] += row[key];
+                        tableData += `<td>${row[key]}</td>`;
+                    }
 
-	                let stateTotal = 0;
-	                for (key in count) {
-	                    stateTotal += count[key];
-	                }
+                    let stateTotal = 0;
+                    for (key in count) {
+                        stateTotal += count[key];
+                    }
 
-	                tableData += `<td>${stateTotal}</td>`;
+                    tableData += `<td>${stateTotal}</td>`;
 
-	                tableData += '</tr>';
-	            });
+                    tableData += '</tr>';
+                });
 
-	            ReadFile(path.join(template_dir, 'state.html')).then(template => {
-	                let response = template;
-					console.log("read template");
-	                //replace the title
-	                response = response.replace('<title>US Energy Consumption</title>', `<title>${currentState} Energy Consumption</title>`);
+                ReadFile(path.join(template_dir, 'state.html')).then(template => {
+                    let response = template;
+                    console.log("read template");
+                    //replace the title
+                    response = response.replace('<title>US Energy Consumption</title>', `<title>${currentState} Energy Consumption</title>`);
 
-	                //replace h2
-	                response = response.replace('<h2>Yearly Snapshot</h2>', `<h2>${currentYear} National Snapshot</h2>`);
+                    //replace h2
+                    response = response.replace('<h2>Yearly Snapshot</h2>', `<h2>${currentYear} National Snapshot</h2>`);
 
-	                //replace the year
-	                response = response.replace('var state;', `var state = ${currentState};`);
+                    //replace the year
+                    response = response.replace('var state;', `var state = ${currentState};`);
 
-	                //replace all of the variables with their new value
-	                for (key in count) {
-	                    response = response.replace(`var ${key}_count;`, `var ${key}_count = ${count[key]};`);
-	                }
+                    //replace all of the variables with their new value
+                    for (key in count) {
+                        response = response.replace(`var ${key}_count;`, `var ${key}_count = ${count[key]};`);
+                    }
 
-	                //get the next/prev state
-	                let states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA",
-	                	"ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA",
-	                	"RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
-	                let prevState;
-	                let nextState;
-	                if(currentState == "AL")
-	                {
-						prevState = "WY";
-						nextState = "AK";
-					}
-					else if(currentState == "WY")
-					{
-						prevState = "WI";
-						nextState = "AL";
-					}
-					else
-					{
-						for(let i = 0; i < states.length; i++)
-						{
-							if(states[i] == currentState)
-							{
-								prevState = states[i-1];
-								nextState = states[i+1];
-							}
-						}
+                    //get the next/prev state
+                    let states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA",
+                        "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA",
+                        "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
+                    let prevState;
+                    let nextState;
+                    if (currentState == "AL") {
+                        prevState = "WY";
+                        nextState = "AK";
+                    }
+                    else if (currentState == "WY") {
+                        prevState = "WI";
+                        nextState = "AL";
+                    }
+                    else {
+                        for (let i = 0; i < states.length; i++) {
+                            if (states[i] == currentState) {
+                                prevState = states[i - 1];
+                                nextState = states[i + 1];
+                            }
+                        }
                     }
 
                     //replace the next and previous buttons
                     response = response.replace(`<a class="prev_next" href="">XX</a> <!-- change XX to prev state, link to WY if state is AK -->`,
-                    `<a class="prev_next" href="/${prevState}">${prevState}</a>`);
+                        `<a class="prev_next" href="/${prevState}">${prevState}</a>`);
 
                     response = response.replace(`<a class="prev_next" href="">XX</a> <!-- change XX to next state, link to AK if state is WY -->`,
-                     `<a class="prev_next" href="/${nextState}">${nextState}</a>`);
-                    
-	                //insert the tableData into the table
-	                response = response.replace('<!-- Data to be inserted here -->', tableData);
+                        `<a class="prev_next" href="/${nextState}">${nextState}</a>`);
 
-	                WriteHtml(res, response);
-	            }).catch((err) => {
-	                Write404Error(res);
-	            });
-	        }
+                    //insert the tableData into the table
+                    response = response.replace('<!-- Data to be inserted here -->', tableData);
+
+                    WriteHtml(res, response);
+                }).catch((err) => {
+                    Write404Error(res);
+                });
+            }
+        });
     });
 });
 
@@ -294,7 +294,7 @@ let WriteCustom404Error = (res, reason) => {
     ReadFile(path.join(template_dir, '404.html'))
         .then(template => {
             template = template.replace('<p class ="error-reason">page not found</p>',
-            `<p class ="error-reason">${reason}</p>`);
+                `<p class ="error-reason">${reason}</p>`);
 
             res.write(template);
             res.end();
